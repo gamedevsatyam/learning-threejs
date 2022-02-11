20b5221d7ddbd2c777e9a6c2d5663e66a50d1579
Satyam Vishwakarma <mavericksatyam@gmail.com>
2022-02-11 13:12:02 +0530

    Learnt about lights in three.js

---
+++ Three.Js/15-Lights/src/script.js
@@ -0,0 +1,171 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight('white', 0.1)
+scene.add(ambientLight)
+
+const hemisphereLight = new THREE.HemisphereLight('blue', 0.1)
+scene.add(hemisphereLight)
+hemisphereLight.position.y = 1.2
+hemisphereLight.position.x = -1.2
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.1)
+scene.add(hemisphereLightHelper)
+
+const pointLight = new THREE.PointLight('red', 0.5)
+scene.add(pointLight)
+pointLight.position.set(0,0,0)
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.3)
+scene.add(pointLightHelper)
+
+const directionLight = new THREE.DirectionalLight('white', 0.5)
+scene.add(directionLight)
+directionLight.intensity = 0.1
+const directionLightHelper = new THREE.DirectionalLightHelper(directionLight, 0.3)
+scene.add(directionLightHelper)
+
+const spotLight = new THREE.SpotLight(0xf0f0f0, 0.7)
+scene.add(spotLight)
+spotLight.angle = Math.PI * 0.2
+spotLight.intensity =0.1
+const spotLightHelper = new THREE.SpotLightHelper(spotLight, 0.3)
+scene.add(spotLightHelper)
+
+
+const rectAreaLight = new THREE.RectAreaLight('white', 1)
+scene.add(rectAreaLight)
+
+
+
+
+// const pointLight = new THREE.PointLight(0xffffff, 0.5)
+// pointLight.position.x = 2
+// pointLight.position.y = 3
+// pointLight.position.z = 4
+// scene.add(pointLight)
+
+/**
+ * Objects
+ */
+// Material
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.4
+
+// Objects
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+)
+sphere.position.x = - 1.5
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 0.75, 0.75),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+    material
+)
+torus.position.x = 1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    material
+)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = - 0.65
+
+scene.add(sphere, cube, torus, plane)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime
+    cube.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    cube.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()

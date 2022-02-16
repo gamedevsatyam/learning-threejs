017f93c6213e67573e0d150e23a9745c75ea0660
Satyam Vishwakarma <mavericksatyam@gmail.com>
2022-02-16 19:46:32 +0530

    Learnt about raycaster and mouse events

---
+++ Three.Js/20-Raycaster/src/script.js
@@ -0,0 +1,150 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const object1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object1.position.x = - 2
+
+const object2 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+
+const object3 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object3.position.x = 2
+
+scene.add(object1, object2, object3)
+
+
+//Raycaster
+const raycaster = new THREE.Raycaster()
+
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event)=>{
+	mouse.x = event.clientX / sizes.width * 2 -1
+	mouse.y = -(event.clientY / sizes.height) * 2 +1
+})
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 4
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+	const elapsedTime = clock.getElapsedTime()
+
+	// Update controls
+	controls.update()
+
+	//animateObject
+	object1.position.y = Math.sin(elapsedTime)
+	object2.position.y = Math.sin(elapsedTime) *2
+	object3.position.y = Math.sin(elapsedTime)*3
+
+	// raycaster.set(
+	// 	new THREE.Vector3(-3,0,0),
+	// 	new THREE.Vector3(1, 0, 0).normalize()
+	// )
+
+	// const objectsToTest = [object1, object2, object3]
+	// const intersects = raycaster.intersectObjects(objectsToTest)
+
+
+	// for (const object of objectsToTest) {
+	// 	object.material.color.set('red')
+	// }
+
+	// for(const intersect of intersects) {
+	// 	intersect.object.material.color.set('white')
+	// }
+
+	raycaster.setFromCamera(mouse, camera)
+	const objectsToTest = [object1, object2, object3]
+	const intersects = raycaster.intersectObjects(objectsToTest)
+
+	for(const object of objectsToTest) {
+		object.material.color.set('red')
+	}
+
+	for (const intersect of intersects) {
+	intersect.object.material.color.set('blue')
+	}
+
+
+	// Render
+	renderer.render(scene, camera)
+
+	// Call tick again on the next frame
+	window.requestAnimationFrame(tick)
+}
+
+tick()
